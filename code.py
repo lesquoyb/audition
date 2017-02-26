3c847d8c908dddff11145cd5d3d817c7ffc68abf
lesquoyb <lesquoy.baptiste@gmail.com>
2017-02-26 16:51:31 +0100

    coucou

--- code.py
+++ code.py
@@ -8,12 +8,16 @@ import leven3
 
 if __name__ == "__main__":
 
-    lex = parseLex("data/lexicon-2syll-0100words.lex")
+    lex = parseLex("data/lexicon-3syll-0500words.lex")
     phonemes = parsePhonemes()
-    test = parseTest("data/test-2syll-0100words.test")
+    test = parseTest("data/test-3syll-0500words.test")
     hmm = parseModeleDiscret()
     print("hmm:")
     leven2.levenshtein_btw_files(lex, test, hmm)
+    print("hmm + apprentissage:")
+    hmm = leven3.learn()
+    leven2.levenshtein_btw_files(lex, test, hmm)
+
     print("normal:")
     leven1.levenshtein_btw_files(lex, test)
 
